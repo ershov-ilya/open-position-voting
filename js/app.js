26cac5aaeb664bbef8892bbdd3c6503975d96666
Илья\ Ершов <ershov.ilya@effetto.pro>
2015-07-07 23:56:21 +0300

    Сервер отвечает

---
+++ js/app.js
@@ -0,0 +1,67 @@
+var sock = new SockJS('http://ershov.pw:8080/vote');
+
+var VOTE=(function(){
+	// Private
+	var countdown=0;
+	var channel=1;
+	function btnEnable(){
+		$('button.btn-vote').removeAttr('disabled');
+		$('#countdown').text(countdown).slideUp();
+	}
+	function tick(){
+		if(countdown>0){
+			$('#countdown').text(countdown);
+			countdown--;
+		}
+	}
+	
+	function listenButtons(){
+		$("button").click(function(e){
+			var vote_action=$(this).data('vote');
+			var channel_action=$(this).data('channel');
+			if(vote_action){
+				PUBLIC.btnDisable();
+				sock.send('{"command":"vote","channel":"'+channel+'","vote":"'+vote_action+'"}');
+			}
+			if(channel_action){
+				if(channel_action=="+1") channel++;
+				if(channel_action=="-1") channel--;
+				if(channel<1) channel=1;
+				if(channel>100) channel=100;
+				$('#channel').text(channel);
+			}
+		});
+	}
+		
+	// Public
+	var PUBLIC = {
+		btnDisable: function(){
+			$('button.btn-vote').attr('disabled','disabled');
+			countdown=10;
+			$('#countdown').text(countdown).slideDown();
+			setTimeout(btnEnable, 10000);
+		},
+		ask: function(){
+			sock.send('{"command":"ask"}');
+		},
+		init:function(config){
+			console.log('VOTE init()');
+			setInterval(tick,1000);
+			listenButtons();
+		}
+	};
+	return PUBLIC;
+})();
+
+sock.onmessage = function(e) {
+ console.log('message', e.data);
+};
+sock.onclose = function() {
+ console.log('close');
+};
+
+
+
+$(document).ready(function(){
+	VOTE.init();
+});
--- server/index.js
+++ server/index.js
@@ -1,14 +1,58 @@
 var http = require('http');
 var sockjs = require('sockjs');
 
+var num=0,
+RATING=[],
+conn_arr=[],
+admin_arr=[];
+
 var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js' });
 echo.on('connection', function(conn) {
+	conn.user_id=num;
+	conn.user_type='user';
+	conn.user_channel=1;
+	conn_arr[num]=conn;
+	num++;
+	
+	conn.write('ID #'+conn.user_id);
+	console.log('new connection #'+conn.user_id);
+	
     conn.on('data', function(message) {
-        conn.write(message);
+		// Ты админ?
+		var data=JSON.parse(message);
+		if(data.command){
+			switch(data.command){
+				case 'admin':
+					admin_arr[num]=conn;
+					conn.user_type='admin';
+					console.log('admin registered: #'+conn.user_id);
+				break;
+				case 'vote':
+					if(data.vote && data.channel){
+						if(typeof RATING[data.channel]=='undefined') RATING[data.channel]=0;
+						
+						if(data.vote=='+1') RATING[data.channel]++;
+						if(data.vote=='-1') RATING[data.channel]--;
+						console.log('channel #'+data.channel+', Rating: '+RATING[data.channel]);
+					}
+				break;
+				case 'ask':
+					if(conn.user_type=='admin'){
+						conn.write('{"rating":"'+RATING[conn.user_channel]+'"}');
+					}
+				break;
+			}
+		}
+		
+		// conn.write(message);
+		//console.log(message);
     });
-    conn.on('close', function() {});
+	
+    conn.on('close', function() {
+		
+	});
 });
 
 var server = http.createServer();
-echo.installHandlers(server, {prefix:'/echo'});
+echo.installHandlers(server, {prefix:'/vote'});
 server.listen(8080, '0.0.0.0');
